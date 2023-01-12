a67e54c906f095d270e92be0620e5d68db6b2900
preyash5 <20bca12@vtcbb.edu.in>
2023-01-12 18:53:58 +0530

    changed

--- workout.php
+++ workout.php
@@ -230,7 +230,7 @@
                     <span class="ml-3 text-xl">Plus Ultra Fitness</span>
                 </a>
                 <p class="text-sm text-gray-400 sm:ml-6 sm:mt-0 mt-4">© 2022 Plus Ultra Fitness —
-                    <a href="https://www.instagram.com/harshiilllll/" class="text-gray-500 ml-1" target="_blank"
+                    <a href="https://www.instagram.com/" class="text-gray-500 ml-1" target="_blank"
                         rel="noopener noreferrer">Preyash|Urvil</a>
                 </p>
                 <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
@@ -248,7 +248,7 @@
                             </path>
                         </svg>
                     </a>
-                    <a href="https://www.instagram.com/harshiilllll/" class="ml-3 text-gray-400">
+                    <a href="https://www.instagram.com/" class="ml-3 text-gray-400">
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                             stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                             <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
